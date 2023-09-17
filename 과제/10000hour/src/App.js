811a1491f28c07e5a9b63ba8424d53561ba49c6a
donguramee <sanulim94@gmail.com>
2023-09-17 21:12:58 +0900

    TIL

--- 과제/10000hour/src/App.js
+++ 과제/10000hour/src/App.js
@@ -1,8 +1,16 @@
+import React from "react";
+import Main from "./components/Main";
+import Result from "./components/Result";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <Main />
+      <Result />
+      <Footer />
+    </>
   );
 }
+
 export default App;
---
+++ 과제/10000hour/src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <h1>
+      <img src="img/frame.svg" alt="10000hour" />
+      <img src="img/clock.svg" alt="clock" />
+    </h1>
+  );
+}
