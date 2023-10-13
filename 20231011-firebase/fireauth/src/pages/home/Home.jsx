18cd545529cbd81bc64b98583b9b92b4a5cff4ec
donguramee <sanulim94@gmail.com>
2023-10-13 20:44:50 +0900

    TIL-firebase

--- 20231011-firebase/fireauth/src/pages/home/Home.jsx
+++ 20231011-firebase/fireauth/src/pages/home/Home.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import styles from "./Home.module.css";
+import DiaryForm from "./DiaryForm";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <main className={styles["diary-main"]}>
+        <h2 className="heart">2023.06.08의 비밀일기</h2>
+        <DiaryForm></DiaryForm>
+      </main>
+      <section>
+        <h2 className="a11y-hidden">일기 목록</h2>
+        <ul></ul>
+      </section>
+    </div>
+  );
 }
